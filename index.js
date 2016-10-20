3b98a5fbda8298964a72ec187eebd4a4ec641819
Celso Takeo Shigaki <ctakeo@ciandt.com>
2016-10-19 22:34:14 -0200

    Bot is now fully functional

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var msgCount = new Array();
 
 timer.setInterval(function() {
   msgCount = new Array();
-}, 20000);
+}, 40000);
 
 var token = '238362601:AAH1PRHLEA9vPlB8vizuUuvQskqIWbrr82M';
 
 
 var TelegramBot = require('node-telegram-bot-api');
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(token, {polling: {timeout: 1, interval: 50}});
 
 // Any kind of message
 bot.on('message', function (message) {
   var chatId = message.chat.id;
   var fromId = message.from.id;
-  console.log(message);
+  //console.log(message);
   if (msgCount[chatId] == undefined)
     msgCount[chatId] = new Array();
   if (msgCount[chatId][fromId] == undefined) {
@@ -31,7 +31,7 @@ bot.on('message', function (message) {
       msgCount[chatId][fromId][message.document.file_id]++;
     }
   }
-  else if (message.document != undefined) {
+  else if (message.sticker != undefined) {
     if (msgCount[chatId][fromId][message.sticker.file_id] == undefined) {
       msgCount[chatId][fromId][message.sticker.file_id] = 0;
     }
@@ -48,15 +48,43 @@ bot.on('message', function (message) {
     }
   }
 
-  if (msgCount[chatId][fromId][message.text] == 10) {
-    bot.sendMessage(chatId, 'Spammer Detected! Name: ' + message.from.first_name);
-    bot.sendMessage(chatId, 'You will be kicked in 3 seconds m8');
-    setTimeout(function() {
-      if (fromId != '126151878') {
-        bot.kickChatMember(chatId, fromId);
-        bot.sendMessage(chatId, 'PRAISE ALLAH');
-        console.log('Just kicked ' + message.from.first_name);
-      }
-    }, 3000)
-  }
+  /*if (message.text != undefined)
+    console.log(msgCount[chatId][fromId][message.text]);
+  if (message.sticker != undefined)
+    console.log(msgCount[chatId][fromId][message.sticker.file_id]);
+  if (message.document != undefined)
+    console.log(msgCount[chatId][fromId][message.document.file_id]);*/
+
+  if (message.text != undefined){
+    if (msgCount[chatId][fromId][message.text] >= 10)
+      kickFaggot(chatId, fromId, message.from.first_name);
+    else if (msgCount[chatId][fromId][message.text] == 5)
+      bot.sendMessage(chatId, 'Stahp ' + message.from.first_name);
+  } else if (message.sticker != undefined) {
+    if (msgCount[chatId][fromId][message.sticker.file_id] >= 10)
+      kickFaggot(chatId, fromId, message.from.first_name);
+    else if (msgCount[chatId][fromId][message.sticker.file_id] == 5)
+      bot.sendMessage(chatId, 'Stahp ' + message.from.first_name);
+  } else if (message.document != undefined)
+    if (msgCount[chatId][fromId][message.document.file_id] >= 10)
+      kickFaggot(chatId, fromId, message.from.first_name);
+    else if (msgCount[chatId][fromId][message.document.file_id] == 5)
+      bot.sendMessage(chatId, 'Stahp ' + message.from.first_name);
 });
+
+function kickFaggot(chatId, fromId, name) {
+  bot.sendMessage(chatId, 'Spammer Detected! Name: ' + name);
+  bot.sendMessage(chatId, 'You will be kicked in 3 seconds m8');
+  timer.setTimeout(function() {
+    if (fromId != '126151878') {
+      bot.kickChatMember(chatId, fromId);
+      bot.sendMessage(chatId, 'PRAISE ALLAH');
+      console.log('Just kicked ' + name);
+
+      timer.setTimeout(function() {
+        bot.unbanChatMember(chatId, fromId);
+        bot.sendMessage(fromId, 'You have been automatically unbanned.')
+      }, 60000);
+    }
+  }, 3000);
+}
